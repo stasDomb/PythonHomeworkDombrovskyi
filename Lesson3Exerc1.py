b4fc4cc23cb179b9dfa047707e6b2aa4c49509d8
stasDomb <salsafollower@gmail.com>
2018-12-15 22:01:54 +0200

    Small changes in implementation of first task Second task for lesson 3 was finished

--- Lesson3Exerc1.py
+++ Lesson3Exerc1.py
@@ -6,8 +6,8 @@ def search_letter(example_string):
     for word in separated_string:
         if word.istitle():
             result_string += word[0]
-    return print(result_string)
+    return result_string
 
 
 example_string = "How are you? Eh, ok. Low or Lower? Ohhh."
-search_letter(example_string)
+print(search_letter(example_string))
