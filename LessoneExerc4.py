45ea1509419ff1ef8fa56c2a225670aa49434e46
stasDomb <salsafollower@gmail.com>
2018-12-16 12:04:50 +0200

    Third and fourth tasks for lesson 3 were finished

---
+++ LessoneExerc4.py
@@ -0,0 +1,11 @@
+
+
+def modify_string(example_string):
+    the_begin = example_string[:1]
+    the_end = example_string[len(example_string) - 1:]
+    new_string = the_end + example_string[1:] + the_begin
+    return new_string
+
+
+example_string = "How. are you? Eh, ok. Low or Lower? Ohhh."
+print(modify_string(example_string))
