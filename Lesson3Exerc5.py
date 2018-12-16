98b5f231a705c8a968eb81d0b9aed91ce6b9f723
stasDomb <salsafollower@gmail.com>
2018-12-16 12:14:33 +0200

    Fifth task for lesson 3 was finished

---
+++ Lesson3Exerc5.py
@@ -0,0 +1,15 @@
+# Задача-5
+# Дан тапл(tuple), необходимо его конвертнуть в стринг.
+# Например:
+# ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's') == 'exercises
+
+
+def modify_tuple_to_str(example_tuple):
+    result_string = ""
+    for i in example_tuple:
+        result_string += i
+    return result_string
+
+
+example_tuple = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+print(modify_tuple_to_str(example_tuple))
